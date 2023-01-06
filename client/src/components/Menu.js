a7001d09d1403da8cb86035df871bca4277f1c16
surajit <surajitch10@gmail.com>
2023-01-06 17:14:16 +0530

    Menu page

--- client/src/components/Menu.js
+++ client/src/components/Menu.js
@@ -1,12 +1,24 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Logo from './Logo'
 import Button from '@mui/material/Button';
 import Menu from '@mui/material/Menu'
 import MenuItem from '@mui/material/MenuItem'
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import { CardActionArea } from '@mui/material';
 import { Link } from 'react-router-dom';
 import {BsChevronDown} from 'react-icons/bs'
+import upload from '../resources/upload.jpeg'
+import electr from '../resources/electr.jpeg'
+import grocery from '../resources/grocery.png'
+import book from '../resources/book.jpg'
 
 function Menus() {
+  useEffect(() =>{
+    document.title = 'Menu'
+  })
 
   const [anchorEl, setAnchorEl] = useState(null);
   const open = Boolean(anchorEl);
@@ -21,67 +33,122 @@ function Menus() {
   return (
     <>
       <Logo/>
-      <div className='row pt-4 mt-4'>
-        <div className='col-3'></div>  
-        <div className='col-2'>
-          <Link to="/upload">
-            <Button
-              style={{
-                  borderRadius: 12,
-                  backgroundColor: "Blue",
-                  padding: "10px",
-                  fontSize: "18px"
-              }}
-              variant="contained"
-            >
-                Upload
-            </Button>
+      <div className='row pt-4 mt-4 ml-3 pb-3'> 
+        <div className='col-3'>
+          <Link to="/upload" style ={{textDecoration: "none"}}>
+            <Card sx={{marginLeft:10, maxWidth: 345 }} style={{textAlign: "center",}}>
+              <CardActionArea >
+                <div className='d-flex justify-content-center'>
+                  <CardMedia
+                    component="img"
+                    backgroundColor="Black"
+                    image={upload}
+                    style={{width:"100px",height:"100px", justifyContent: "center", display: "flex" }}
+                    className="img-fluid"
+                    alt="upload"
+                  />
+                </div>
+                
+                <CardContent>
+                  <Typography gutterBottom variant="h5" component="div">
+                    Upload
+                  </Typography>
+                  <Typography variant="body2" color="text.secondary">
+                    Upload Your files here
+                  </Typography>
+                </CardContent>
+              </CardActionArea>
+            </Card>
           </Link>
         </div>  
-        <div className='col-3'></div>  
-        <div className='col-2'>
-          <Button
-            style={{
-              borderRadius: 12,
-              backgroundColor: "Blue",
-              padding: "10px",
-              fontSize: "18px",
-              color:"#fff"
-            }}
-            id="basic-button"
-            aria-controls={open ? 'basic-menu' : undefined}
-            aria-haspopup="true"
-            aria-expanded={open ? 'true' : undefined}
-            onClick={handleClick}
-          >
-            Items <BsChevronDown/>
-          </Button>
-          <Menu
-            id="basic-menu"
-            anchorEl={anchorEl}
-            open={open}
-            onClose={handleClose}
-            MenuListProps={{
-              'aria-labelledby': 'basic-button',
-            }}
-          >
-            <Link style ={{textDecoration: "none"}} to="/submenu/1">
-              <MenuItem >Item 1</MenuItem>
-            </Link>
-            <Link style ={{textDecoration: "none"}} to="/submenu/2">
-              <MenuItem >Item 2</MenuItem>
-            </Link>
-            <Link style ={{textDecoration: "none"}} to="/submenu/3">
-              <MenuItem >Item 3</MenuItem>
-            </Link>
-          </Menu>
+        <div className='col-3'>
+          <Link to="/submenu/1" style ={{textDecoration: "none"}}>
+            <Card sx={{ maxWidth: 345 }} style={{textAlign: "center" }}>
+              <CardActionArea>
+                <div className='d-flex justify-content-center'>
+                  <CardMedia
+                    component="img"
+                    backgroundColor="Black"
+                    image={electr}
+                    style={{width:"100px",height:"100px"}}
+                    className="img-fluid"
+                    alt="upload"
+                  />
+                </div>
+                
+                <CardContent>
+                  <Typography gutterBottom variant="h5" component="div">
+                    Electonics
+                  </Typography>
+                  <Typography variant="body2" color="text.secondary">
+                    Click to see the electronics items
+                  </Typography>
+                </CardContent>
+              </CardActionArea>
+            </Card>
+          </Link>  
         </div>  
-        <div className='col-2'></div>  
+
+        <div className='col-3'>
+          <Link to="/submenu/2" style ={{textDecoration: "none"}}>
+            <Card sx={{ maxWidth: 345 }} style={{textAlign: "center" }}>
+              <CardActionArea>
+
+              <div className='d-flex justify-content-center'>
+                <CardMedia
+                  component="img"
+                  backgroundColor="Black"
+                  image={grocery}
+                  style={{width:"100px",height:"100px"}}
+                  className="img-fluid"
+                  alt="upload"
+                />
+              </div>
+                
+                <CardContent>
+                  <Typography gutterBottom variant="h5" component="div">
+                    Grocery
+                  </Typography>
+                  <Typography variant="body2" color="text.secondary">
+                    Click to see the grocery items
+                  </Typography>
+                </CardContent>
+              </CardActionArea>
+            </Card>
+          </Link>  
+        </div>  
+        
+        <div className='col-3'>
+          <Link to="/submenu/3" style ={{textDecoration: "none"}}>
+            <Card sx={{ maxWidth: 345 }} style={{textAlign: "center" }}>
+              <CardActionArea>
+              <div className='d-flex justify-content-center'>
+                <CardMedia
+                  component="img"
+                  backgroundColor="Black"
+                  image={book}
+                  style={{width:"100px",height:"100px"}}
+                  className="img-fluid"
+                  alt="upload"
+                />
+              </div>
+                
+                <CardContent>
+                  <Typography gutterBottom variant="h5" component="div">
+                    Books
+                  </Typography>
+                  <Typography variant="body2" color="text.secondary">
+                    Click to see the Books
+                  </Typography>
+                </CardContent>
+              </CardActionArea>
+            </Card>
+          </Link>  
+        </div>  
 
       </div> 
 
       <div className='d-flex justify-content-center pt-4 mt-4'
-        style={{position: "absolute", top:"60%", left:"47%"}}
       >
       <Button
         style={{
--- client/src/components/Upload.js
+++ client/src/components/Upload.js
@@ -1,8 +1,23 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import Button from '@mui/material/Button';
+import Logo from './Logo'
 
 function Upload() {
+  useEffect(() =>{
+    document.title = 'Upload'
+  })
+
   return (
-    <div>Upload</div>
+    <>
+      <Logo/>
+      <div className='d-flex justify-content-center pt-3 mt-3'>
+        <input type="file"/>
+      </div>
+
+      <div className='d-flex justify-content-center pt-3 mt-3'>
+        <Button variant="contained">Upload</Button>
+      </div>
+    </>
   )
 }
 
